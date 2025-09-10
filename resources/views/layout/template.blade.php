774f1857c8e43d8bb4ae8d1afd2822f080bb2d69
rifqaahmadzaruq@gmail.com <125125672+RifqaAhmad-Zaruq@users.noreply.github.com>
2025-09-10 17:45:10 +0100

    Updated seeders and views to enhance user roles and comment display.

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -156,6 +156,19 @@
         <script src="{{ asset('assets/js/pages/widgets.js') }}"></script>
         <!--end::Page Scripts-->
         @stack('scripts')
+        <script type="text/javascript">
+        (function () {
+            var options = {
+                whatsapp: "+2349036879449", // WhatsApp number
+                call_to_action: "Message us", // Call to action
+                position: "left", // Position may be 'right' or 'left'
+            };
+            var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+            var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+            s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+            var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+        })();
+    </script>
     </body>
     <!--end::Body-->
 </html>
